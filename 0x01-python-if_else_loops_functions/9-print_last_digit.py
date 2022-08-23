0cf70b6d7a0813e6d4acc6b593511f69cc8c0e89
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-23 22:25:13 +0000

    task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,6 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = number % -10
+        last_digit = -1 * last_digit
     print("{:d}".format(last_digit), end="")
     return(last_digit)
