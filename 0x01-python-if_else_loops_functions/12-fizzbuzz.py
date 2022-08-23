d87e7139d0054fffa3ec4a77024bc2d39b2e0f20
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-23 22:58:03 +0000

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3 == 0):
+            number = 'Fizz'
+        elif (number % 5 == 0):
+            number = 'Buzz'
+        elif (number % 15 == 0):
+            number = 'FizzBuzz'
+        print("{}".format(number), end=" ")
