cd829b23632af15056604cb0d84d211f89ee96aa
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-24 23:14:18 +0000

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks linked lists
+ * if the are cycle or not
+ * @list: pointer to the linked list
+ * Return: 0 if theres no cycle and 1 if theres a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr;
+
+	if (list == NULL)
+		return (-1);
+	ptr = list->next;
+	while (ptr != NULL && ptr != list)
+	{
+		ptr = ptr->next;
+		if (ptr == list)
+			return (1);
+		else
+			return (0);
+	}
+}
