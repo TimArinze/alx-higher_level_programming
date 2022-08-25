c362ca2371ac2d4d02a5ae928f288b7da0580c0e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-25 17:11:23 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-arg_count = len(argv) - 1
-if (arg_count == 0):
-    print("{:d} arguments.".format(arg_count))
-elif (arg_count == 1):
-    print("{:d} argument:\n{:d}: {}".format(arg_count, arg_count, argv[1]))
-elif (arg_count > 1):
-    print("{:d} arguments:".format(arg_count))
-    i = 1
-    while (i <= arg_count):
-        print("{:d}: {:s}".format(i, argv[i]))
-        i = i + 1
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    arg_count = len(argv) - 1
+    if (arg_count == 0):
+        print("{:d} arguments.".format(arg_count))
+    elif (arg_count == 1):
+        print("{:d} argument:\n{:d}: {}".format(arg_count, arg_count, argv[1]))
+    elif (arg_count > 1):
+        print("{:d} arguments:".format(arg_count))
+        i = 1
+        while (i <= arg_count):
+            print("{:d}: {:s}".format(i, argv[i]))
+            i = i + 1
