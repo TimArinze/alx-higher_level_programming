6c3bb2a661e7917e1287e640ed73adfc4f3d73ee
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-29 07:14:20 -0700

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,10 @@ def element_at(my_list, idx):
         return None
     elif idx > length -1:
         return None
-    for i, s in enumerate(my_list):
-         if i == idx:
-            return s
+    elif 0 <= idx < length:
+        for i, s in enumerate(my_list):
+             if i == idx:
+                return s
+    else:
+        return None
 
