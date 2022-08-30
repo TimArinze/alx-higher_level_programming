3c9c6c7bda5bdf2ec9d82e65918eab533e56dfbb
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-29 19:55:19 -0700

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            print("{}".format(i))
+    for row in matrix:
+        for element in range(len(row)):
+            print("{:d}".format(row[element]),
+                    end="" if element == len(row) -1 else " ")
+        print("")
