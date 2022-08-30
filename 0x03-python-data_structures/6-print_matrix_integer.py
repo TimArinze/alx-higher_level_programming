7012ccec45d6a9435334aaf193d8bbaba9c354e5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-29 20:07:10 -0700

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in range(len(row)):
-            print("{:d}".format(row[column]),
+            print("{:d}".format(row[column]),\
                     end="" if column == len(row) - 1 else " ")
         print("")
