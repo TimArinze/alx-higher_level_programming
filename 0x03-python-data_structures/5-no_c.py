136142280f994ac8337a438ebc9e7029624615a3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-29 19:22:22 -0700

    task6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if my_string:
         while 'c' in my_string:
-            my_string.remove('c')
+            my_string.remove(c)
         while 'C' in my_string:
-            my_string.remove('C')
+            my_string.remove(C)
         return (my_string)
