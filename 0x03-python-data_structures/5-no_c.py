9a249dac56800d88aaabd5fac58b57b4236f1320
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-29 19:35:14 -0700

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        change = {ord('c'): None, ord('C'): None}
-        return (my_string.translate(change))
+    change = {ord('c'): None, ord('C'): None}
+    return (my_string.translate(change))
