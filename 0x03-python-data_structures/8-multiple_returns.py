b37ec98a1edbf76e9d1704f9608141a55a18ee5c
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-30 20:41:39 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         first = None
-    first = sentence[0][0]
+    else:
+        first = sentence[0][0]
     return (length, first)
