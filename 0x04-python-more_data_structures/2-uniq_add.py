36485f91fda3771180c888d928bf2b8f32086b8f
Arinze Nwutobo <tim.arinze@outlook.com>
2022-08-31 12:06:38 -0700

    task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        my_list.sort()
-        i, add = 1, my_list[0]
-        while i in range(len(my_list)):
-            if my_list[i] != my_list[i - 1]:
-                add = add + my_list[i]
-                i = i + 1
-        return add
+    return sum(set(my_list))
