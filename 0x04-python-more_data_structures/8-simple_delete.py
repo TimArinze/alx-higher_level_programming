d109354296893364431eec057f18950d7cf1ab48
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-03 23:18:21 +0000

    task

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary:
-        del a_dictionary[key]
-        return (a_dictionary)
+        for i in a_dictionary:
+            if i == key:
+                del i
+    return (a_dictionary)
