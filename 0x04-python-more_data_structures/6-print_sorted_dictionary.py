ebef3e0b8697f5a945221a58c5aff2b91640a499
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-03 16:39:51 +0000

    task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary.sort()
-    for i in a_dictionary:
-        print(i)
+
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))
