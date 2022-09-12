a00c3f9ef385d560d0481f987276cc72a2487b9a
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-12 18:34:09 +0000

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     elements = 0
-    for index in range(0, x):
-        try:
+
+    try:
+        for index in range(0, x):
             print(my_list[index], end="")
             elements += 1;
-        except Exception as IndexError:
-            continue
-    print("")
-    return elements
+        print()
+        return elements
+    except Exception as IndexError:
+        print()
+        return elements
