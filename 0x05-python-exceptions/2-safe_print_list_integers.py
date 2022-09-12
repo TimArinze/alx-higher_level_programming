9a71e7e8d225f49a61f14c75edc5ac363bef583e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-12 19:15:26 +0000

    task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             count += 1
-        except (BaseException, TypeError, IndexError):
-            continue
+        except IndexError:
+            break
+        except Exception:
+            pass
     print()
     return count
