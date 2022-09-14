38fc99e0e0fe677df461c8fc63cde6c89cc121e8
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-13 18:42:12 -0700

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Working on oop with python"""
+""" Working on oop with python """
 class Square:
-    """ Square as a shape."""
+    """ Class representing a square"""
     def __init__(self, size):
         """ initialize the class
+
         Args:
             size (int): size of the square
         """
