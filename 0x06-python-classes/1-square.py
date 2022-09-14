03f564aed78041f5ff027d13b6228b8ebf00bed9
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-13 18:28:23 -0700

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a square class and initialises its size
+"""
 class Square:
-    """ Square as a shape. """
+    """ Square as a shape.
+    """
     def __init__(self, size):
         self.size = size
