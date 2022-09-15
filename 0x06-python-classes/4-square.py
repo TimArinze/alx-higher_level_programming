2e9e5d00b308689bc9c95216966f4baf026fc736
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-15 02:45:10 +0000

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,14 +6,19 @@ class Square:
     """ A class Square that defines a square based on previous Square"""
 
     def __init__(self, size=0):
-        self.size(value)
+        self.__size = size
+
+    @property
     def size(self):
         return self.__size
+
+    @size.setter
     def size(self, value):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
     def area(self):
         return self.__size ** 2
