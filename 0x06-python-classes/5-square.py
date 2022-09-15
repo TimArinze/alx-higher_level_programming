08acf2ccc499fe758d0dfdfe16e2e8aafe6dd3b7
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-15 03:08:38 +0000

    task

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Printing a square """
+
+
+class Square:
+    """ A class that defines a square by 4-square.py"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        value = '#'
+        if self.__size = 0:
+            print()
+        for i in range(size.__size):
+            print(value * size.__size)
