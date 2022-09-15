cc7a9be78759c1b24d3e185eb0f69d404d126a10
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-15 02:50:01 +0000

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(size) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
