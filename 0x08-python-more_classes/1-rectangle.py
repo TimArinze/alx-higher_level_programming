3a0bf388479c2fc774b8e7de20813aa8beb47e67
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-19 18:56:18 +0000

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@ Real Definition of a rectangle
 class Rectangle:
     """ Perfect Description """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
     @property
     def width(self):
         return self.__width
