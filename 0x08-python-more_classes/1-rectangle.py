51f916537869fd58f7e7c276ee22d05fc12cf03e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-19 18:59:55 +0000

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,30 +2,35 @@
 """
 Real Definition of a rectangle
 """
+
+
 class Rectangle:
     """ Perfect Description """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
-        if type(self.__width) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
         return self.__height
     @height.setter
     def height(self, value):
-        if type(self.__height) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
