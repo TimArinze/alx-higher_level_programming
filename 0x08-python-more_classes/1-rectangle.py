7068700c3c42c35413834255d5dcb0dd989b39ce
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-19 18:53:43 +0000

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,11 +6,11 @@ class Rectangle:
     """ Perfect Description """
     def __init__(self, width=0, height=0):
         self.__width = width
-        self.__height = heigth
+        self.__height = height
+    @property
     def width(self):
         return self.__width
-    def height(self):
-        return self.__height
+    @width.setter
     def width(self, value):
         if type(self.__width) != int:
             raise TypeError("width must be an integer")
@@ -18,6 +18,10 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
     def height(self, value):
         if type(self.__height) != int:
             raise TypeError("height must be an integer")
