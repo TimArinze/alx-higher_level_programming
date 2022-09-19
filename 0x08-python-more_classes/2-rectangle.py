69262c446aab66f0d0a6910ca09687a88285719b
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-19 21:01:00 +0000

    task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,7 +9,11 @@ class Rectangle:
     """ Area and Perimeter """
 
     def __init__(self, width=0, height=0):
-        """Instantiation with optional width and height"""
+        """Instantiation with optional width and height
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle
+        """
 
         self.width = width
         self.height = height
