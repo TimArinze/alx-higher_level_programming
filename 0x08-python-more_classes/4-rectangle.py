e339248a0c99ea8e60ccb2ce278d1e8d4e09e8e8
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-20 02:44:37 +0000

    task

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -7,10 +7,50 @@ A class Rectangle that defines a rectangle by 3-rectangle
 class Rectangle:
     """ Eval is magic """
 
-    def __init__(self):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
+    def __str__(self):
+        total = ""
+        if self.__width or self.__height == 0:
+            return total
+        for i in range(self.__height):
+            total += ("#" * self.__width)
+            if i != self.__height - 1:
+                total += "\n"
+        return total
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
     @property
     def width(self):
-        return
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
