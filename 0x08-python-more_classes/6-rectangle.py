f7576e186f776f3086b546861918d0abd1856199
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-21 20:37:29 +0000

    task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     """How many instances"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Instantiation"""
         type(self).number_of_instances += 1
