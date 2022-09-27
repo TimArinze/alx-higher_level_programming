3cb5a16759b0727bef40f499fac29000a0fc3dd3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-26 19:04:49 -0700

    task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """A function that returns True or False """
+
+    return isinstance(obj, a_class)
