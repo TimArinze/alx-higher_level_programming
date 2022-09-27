a2026663f6455cce8bf012f2650c4169039a3846
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-27 22:53:06 +0000

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,7 +7,7 @@ using a JSON representation
 
 def save_to_json_file(my_obj, filename):
     """Save Object to a file"""
-    
+
     import json
     with open(filename, "w") as f:
         json.dump(my_obj, f)
