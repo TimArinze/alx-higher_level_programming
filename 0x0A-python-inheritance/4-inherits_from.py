381fc4b0aef311c63aed79254baac40e52aaf003
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-26 19:30:07 -0700

    task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns True if the object is an instance
+of a class that inherited from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Only sub class of"""
+
+    return issubclass(obj, a_class)
