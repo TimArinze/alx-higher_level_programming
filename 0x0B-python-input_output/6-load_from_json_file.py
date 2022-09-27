921b26d751e8c2665727aee26a08b546e5bc2fc6
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-27 23:08:40 +0000

    task

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A function that creates an object from a "JSON file"
+"""
+
+
+def load_from_json_file(filename):
+    """Create object from a JSON file"""
+
+    import json
+    from io import StringIO
+    with open(filename, "r") as f:
+        json.loads(StringIO(f))
