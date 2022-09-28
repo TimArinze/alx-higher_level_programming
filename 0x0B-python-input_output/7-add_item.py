25bd07c4a6c894f7358c5f09223d27f9c090e908
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-28 16:34:13 +0000

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,14 @@ and then save them to a file
 
 
 import sys
-import json
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 file_name = "add_item.json"
-my_list = args[1:]
-
-with open(file_name, "w+") as f:
-    save_to_json_file(my_list, file_name)
-    load_from_json_file(file_name)
+try:
+    my_list = load_from_json_file(file_name)
+except FileNotFoundError:
+    my_list = []
+my_list.extend(args[1:])
+save_to_json_file(my_list, file_name)
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student
+"""
+
+
+class Student:
+    """ Student to JSON """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return Student.__dict__
