c2f433f969f126a144cb3daf0a8cd5088ed2a1e3
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-28 00:59:20 +0000

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,6 +14,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv
 file_name = "add_item.json"
 my_list = args[1:]
+
 with open(file_name, "a+") as f:
     save_to_json_file(my_list, file_name)
     load_from_json_file(file_name)
