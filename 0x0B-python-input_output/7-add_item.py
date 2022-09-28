985e312a30ab57498e9b31cd253889ff008153fd
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-28 00:49:55 +0000

    task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv
+file_name = "add_item.json"
+my_list = []
+my_list.append(args[1:])
+with open(file_name, "a+") as f:
+    save_to_json_file(my_list, file_name)
+    load_from_json_file(file_name)
