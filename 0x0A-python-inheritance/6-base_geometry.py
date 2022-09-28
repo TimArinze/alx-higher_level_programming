53195334beac3b6646ac266f981f4e4c458d31e1
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-28 15:04:57 +0000

    task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry base on 5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """ Improve Geometry """
+
+    def area(self):
+        raise Exception("area() is not implemented")
