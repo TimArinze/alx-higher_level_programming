fe613068b2a8fc2d0bee21287ff4daa0433f2731
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-29 00:59:11 +0000

    task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A function that adds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """ Integers addition """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be integer")
+    elif type(a) == float:
+        a = int(a)
+    elif type(b) == float:
+        b = int(b)
+    return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod("tests/0-add_integer.txt")
