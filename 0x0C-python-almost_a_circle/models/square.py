df897979248f91f50f4e591842609129cb6e9aba
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-10-03 20:11:30 +0000

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,8 +4,15 @@ And now, the Square!
 """
 
 
+from models.rectangle import Rectangle
+
 class Square(Rectangle):
     """A class Square that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        Rectangle
+        """initializing the square"""
+        Rectangle.__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """__str__"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
