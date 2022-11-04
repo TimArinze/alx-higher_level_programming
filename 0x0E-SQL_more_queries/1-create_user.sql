c6b69d0bb98b22cc6f4d4982c909a9913f8c303f
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-03 17:46:03 -0700

    task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,5 +5,4 @@
 -- If the user user_0d_1 already exists, your script should not fail
 USE mysql;
 CREATE USER IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL ON * . * TO 'user_0d_1'@'localhost';
