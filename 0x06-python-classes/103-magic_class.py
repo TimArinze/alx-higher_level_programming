b72702b19ae153d630450402a7e3162784580653
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-28 03:19:03 +0100

    task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-import math
+
 """
 The python class MagicClass does same with the python bytecode
 """
+
+import math
+
 
 class MagicClass:
     """Magic class"""
 
     def __init__(self, radius=0):
         """instantiation"""
-        self.radius = radius
-
-    @property
-    def radius(self):
-        """getter"""
-        return self.__radius
-
-    @radius.setter
-    def radius(self, value):
-        """setter"""
-        if type(value) is not int and type(value) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.__radius = value
+        self.__radius = radius
 
     def area(self):
         """area"""
-        return math.pi * (self.radius ** 2)
+        return math.pi * (self.__radius ** 2)
 
     def circumference(self):
         """circumference"""
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
