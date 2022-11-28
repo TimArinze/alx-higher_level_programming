69091d837ed530225dd564ca7cc58f0390a69734
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-28 22:18:36 +0100

    task

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* A script that prints a message depending on the number of arguments passed
+ */
+// Importing the process module
+const process = require('process');
+
+// Printing process.argv property value
+const args = process.argv;
+
+if (args.length < 3)
+{
+	console.log('No Argument');
+}
+else if (args.length > 3)
+{
+	console.log('Arguments found');
+}
+else
+{
+	console.log('Argument found');
+}
