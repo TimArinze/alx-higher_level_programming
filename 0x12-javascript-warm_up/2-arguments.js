50e3131ea48dd66bb89878f9edd227f5554c6fbb
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-28 22:20:47 +0100

    task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,15 +7,10 @@ const process = require('process');
 // Printing process.argv property value
 const args = process.argv;
 
-if (args.length < 3)
-{
-	console.log('No Argument');
-}
-else if (args.length > 3)
-{
-	console.log('Arguments found');
-}
-else
-{
-	console.log('Argument found');
+if (args.length < 3) {
+  console.log('No Argument');
+} else if (args.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('Argument found');
 }
