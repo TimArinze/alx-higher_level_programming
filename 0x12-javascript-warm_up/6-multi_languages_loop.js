f17ac8aacf90e2d647e34337fcc8c0f8a312808a
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-28 23:22:31 +0100

    task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* 3 lines using array of string and a loop
+ */
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
