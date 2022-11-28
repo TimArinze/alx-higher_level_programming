c16e73f99edd3f29101a991ecb668feab99eb501
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-28 23:32:13 +0100

    task

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* A script that prints x times"C is fun"
+ */
+const process = require('process');
+
+const args = process.argv;
+
+const num = parseInt(args[2]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
