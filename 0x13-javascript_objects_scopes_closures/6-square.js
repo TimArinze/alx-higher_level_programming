4abef19430976c3fb984929ca5bde6ab3a334600
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-29 22:13:00 +0100

    task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,13 @@
 /* Square that defines a square and inherits from Square of 5-square.js
  */
 module.exports = class Square extends require('./5-square') {
-	charPrint (c) {
-		if (c === undefined) {
-			this.print();
-		} else {
-			for (let i = 0; i < this.height; i++) {
-				console.log(c.repeat(this.height));
-			}
-		}
-	}
-}
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+};
