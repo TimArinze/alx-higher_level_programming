f2da19837641ad33727c833a2b4a446269a48ca5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-29 20:33:28 +0100

    task

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* rectangle printing class
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const string = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(string.repeat(this.width));
+    }
+  }
+};
