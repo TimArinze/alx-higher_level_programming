6a6441d661dce861b73f8fb306b355103ba1c527
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-29 18:59:08 +0100

    task

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,8 +2,8 @@
 /* A class Rectangle that defines a rectangle
  */
 module.exports = class Rectangle {
-	constructor(w, h) {
-		this.w = w;
-		this.h = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
