74bad9c42334fbec44ad9568f513494c60c07343
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-29 20:59:35 +0100

    task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/* class that rotates double and prints
+ */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const string = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(string.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
