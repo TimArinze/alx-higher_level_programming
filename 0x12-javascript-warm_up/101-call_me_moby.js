b62930cb8283a19710f597bdbef940da71121879
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-30 19:16:31 +0100

    task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,6 +2,5 @@
 /* A function that executes x times a function
  */
 exports.callMeMoby = function (x, theFunction) {
-	for (let i = 0; i < x; i++)
-		theFunction();
-}
+  for (let i = 0; i < x; i++) { theFunction(); }
+};
