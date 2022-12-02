bbf7dc35801b0071a34960d4f8e30307dea0414d
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-02 16:51:02 +0100

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+
 
 import sys
 import MySQLdb
+
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
@@ -12,6 +18,3 @@ if __name__ == "__main__":
     for row in query_rows:
         if row[1][0] == "N":
             print(row)
-cur.close()
-db.close()
-
