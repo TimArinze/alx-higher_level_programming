a243710b7c3321dd92c5882fcefd8f5bda301ba5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-03 15:17:00 +0100

    task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                          host="localhost", port=3306)
     cur = db.cursor()
-    cur.execute("SELECT  FROM states GROUP BY cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
