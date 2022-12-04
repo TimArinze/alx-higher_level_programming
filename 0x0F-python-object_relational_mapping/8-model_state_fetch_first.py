d497a7d20a28250814222e20f9e751b236c83440
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-04 23:55:13 +0100

    task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id).filter(State.id == 1):
         # HERE: no sql query, only objects
-        if state.id == 1:
-            print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
