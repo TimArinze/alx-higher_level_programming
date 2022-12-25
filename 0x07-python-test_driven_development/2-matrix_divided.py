9b83b75f69e015131af9c658e0cc0d07d212a822
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-25 03:51:01 +0100

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,14 +11,14 @@ def matrix_divided(matrix, div):
     if type(matrix) is not list:
         raise TypeError(errmsg)
 
-    if type(div) is not int and type(div) is not float:
+    elif type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         if type(row) is not list:
             raise TypeError(errmsg)
-        if len(row) is not len(matrix[0]):
+        elif len(row) is not len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if type(element) is not int and type(element) is not float:
