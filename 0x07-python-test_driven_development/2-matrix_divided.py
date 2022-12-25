6b6a24d8c84bbf16b0c99b9b70734c1d42983627
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-25 03:39:28 +0100

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,14 +8,12 @@ def matrix_divided(matrix, div):
     """ Divide a matrix """
 
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lis\
-                ts) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
     for row in matrix:
         if type(row) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
