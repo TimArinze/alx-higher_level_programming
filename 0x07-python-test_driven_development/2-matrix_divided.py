6b7458288233ed5ffc36e62404174b6dfe3092aa
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-25 03:58:19 +0100

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,17 +8,16 @@ def matrix_divided(matrix, div):
     """ Divide a matrix """
 
     errmsg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list:
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError(errmsg)
-
-    elif type(div) is not int and type(div) is not float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    elif div == 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         if type(row) is not list:
             raise TypeError(errmsg)
-        elif len(row) is not len(matrix[0]):
+        if len(row) is not len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if type(element) is not int and type(element) is not float:
