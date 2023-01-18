a673d06d7c1ff2a98b8449c22e26919101f2d4e3
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-18 14:19:14 +0100

    task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,14 +7,28 @@ const args = process.argv[2];
 
 const request = require('request');
 
-const MAIN_URL = 'https://swapi-api.alx-tools.com/api/people/18/';
+const MAIN_URL = args;
+const CHARACTER_URL = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
 
 request.get(
   MAIN_URL,
   function (error, response, body) {
-    if (error && !args) {
+    if (error) {
       console.error(error);
     }
-    console.log(JSON.parse(body).films.length);
+    const outcome = JSON.parse(body).results;
+    for (let a = 0; a < outcome.length; a++) {
+      for (const [key, value] of Object.entries(outcome[a])) {
+        if (key === 'characters') {
+          for (let b = 0; b < value.length; b++) {
+            if (value[b] === CHARACTER_URL) {
+              count += 1;
+            }
+          }
+        }
+      }
+    }
+    console.log(count);
   }
 );
