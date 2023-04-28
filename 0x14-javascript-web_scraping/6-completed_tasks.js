4dd6492f532cc76b24ef831f40591bcb3c72ca9d
Arinze Nwutobo <tim.arinze@outlook.com>
2023-04-28 02:17:31 +0100

    task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,16 +7,15 @@ const args = process.argv[2];
 
 const request = require('request');
 
-
 request.get(
   args,
   function (error, response, body) {
     if (error) {
-    console.error(error);
+      console.error(error);
     }
     const outcome = JSON.parse(body);
     const users = outcome
-      .filter(task => task.completed == true);
+      .filter(task => task.completed === true);
     const reduced = users.reduce((accumulator, user) => {
       if (user.userId in accumulator) {
         accumulator[user.userId] += 1;
